44259d621dc59b26f691ff69616103a91fe52ea7
Michael Dennis <michaelsdennis4@gmail.com>
2015-11-02 07:54:22 -0500

    basic app running locally

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // app.get('stylesheets/style.css', function(req, res) {
 // 	res.sendFile('stylesheets/style.css');
